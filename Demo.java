7a93f39fa321d809f8d12fc2b5cce4f37a74f9cf
prathima3022 <prathimagopavarapu@gmail.com>
2024-08-21 16:02:03 -0500

    changes made in Demo.java update m1()

--- Demo.java
+++ Demo.java
@@ -1,6 +1,6 @@
-public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World");
+class Demo{
+public void m1 {
+    System.out.println("Hey inside m1()");
   }
 }
 
